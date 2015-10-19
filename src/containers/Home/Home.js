e0a01d33335891f6e56a6840dc7182d7fc58b0ae
Jakub Wolny <jakub.wolny@centralway.com>
2015-10-19 18:19:45 +0200

    Updated reducer

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,7 +4,7 @@ import { CounterButton } from 'components';
 
 export default class Home extends Component {
   render() {
-    const styles = require('./Home.scss');
+    const styles = require('./Home.css');
     // require the logo image both from client and server
     const logoImage = require('./logo.png');
     return (
